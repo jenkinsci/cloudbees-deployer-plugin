a5fb8bdfb6c13f54b0cfc46b098aceed56090a7f
Olivier Lamy <olamy@apache.org>
2011-04-17 17:41:39 +0200

    unit test with some asserts it's better :-)

--- src/test/java/org/jenkins/plugins/cloudbees/CloudbeesDeployWarTest.java
+++ src/test/java/org/jenkins/plugins/cloudbees/CloudbeesDeployWarTest.java
@@ -5,6 +5,7 @@ import hudson.maven.MavenModuleSetBuild;
 import hudson.model.Cause;
 import hudson.tasks.Maven;
 import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.lang.StringUtils;
 import org.jvnet.hudson.test.ExtractResourceSCM;
 
 /**
@@ -25,7 +26,26 @@ public class CloudbeesDeployWarTest extends AbstractCloudbeesDeployerPluginTest
         m.getPublishers().add( new CloudbeesPublisher("olamy", "foo/beer", null) );
         MavenModuleSetBuild mmsb =  buildAndAssertSuccess(m);
         for(FileItem fileItem : cloudbeesServer.cloudbessServlet.items) {
-            System.out.println(" item " + fileItem );
+
+            //archive check it's a war content
+
+            //description check Jenkins BUILD_ID
+            if (fileItem.getFieldName().equals( "description" )) {
+                String description = fileItem.getString();
+                // sample Jenkins build 2011-04-17_17-35-05
+                assertTrue( description.contains( "Jenkins build" ) );
+                String id = StringUtils.substringAfter(description, "Jenkins build" ).trim();
+                // TODO assert it's a date time with a parsing !!
+                System.out.println("id : " + id);
+            } else if (fileItem.getFieldName().equals( "api_key" )) {
+                assertEquals( "key",  fileItem.getString() );
+            } else if (fileItem.getFieldName().equals( "app_id" )) {
+                assertEquals( "foo/beer",  fileItem.getString() );
+            } else {
+                System.out.println(" item " + fileItem );
+            }
+
         }
+
     }
 }
